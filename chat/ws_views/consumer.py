1c68be86a6c17a39cd6205edeb73f47e9be6fb5a
VishalMauryastp <vishalmaurya12nov@gmail.com>
2025-06-08 13:21:50 +0530

    Set up core Django project structure with chat app and WebSocket support

---
+++ chat/ws_views/consumer.py
@@ -0,0 +1,11 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({"message": "WebSocket connected"}))
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.send(text_data=json.dumps({"message": data["message"]}))
